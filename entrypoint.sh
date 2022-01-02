3e05f2aea021da897c21c12cbe48b64e37503417
shhamil <kurban0595@inbox.ru>
2022-01-02 17:48:05 +0300

    entrypoint error3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-python manage.py makemigrations
-python manage.py migrate
-python manage.py loaddata extrasens.json
-python manage.py collectstatic --no-input
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py loaddata extrasens.json
+python3 manage.py collectstatic --no-input
 exec gunicorn config.wsgi:application -b 0.0.0.0:8000 --reload
