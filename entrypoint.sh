96dad188e23103b9934f6c1ffc478cb7fb58f7ed
shhamil <kurban0595@inbox.ru>
2022-01-02 21:16:37 +0300

    del port

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py loaddata extrasens.json
 python manage.py collectstatic --no-input
-exec gunicorn config.wsgi:application -b 0.0.0.0:8000 --reload
+exec gunicorn config.wsgi:application -b 0.0.0.0:80 --reload
