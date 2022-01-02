7e592553d1b84c9c24de1891afc9e41c46697570
shhamil <kurban0595@inbox.ru>
2022-01-02 19:44:19 +0300

    asd

--- config/settings.py
+++ config/settings.py
@@ -78,12 +78,8 @@ EXTRASENS_GUESS = 'extrasens_guess'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'xmendb',
-        'USER': 'shamil',
-        'PASSWORD': 'shamil1998',
-        'HOST': 'postgres',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
